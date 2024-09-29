92812d6e932923d2e7adc5c000f1f48b09f962a8
clement <clement.legoube@gmail.com>
2024-09-29 23:45:40 +0200

    Scroll Indicator ; Footer small ; ID & Sections Links

--- js/accueil.js
+++ js/accueil.js
@@ -1,3 +1,80 @@
+// SCROLL INDICATOR
+
+function updateScrollIndicator() {
+    const totalHeight = document.documentElement.scrollHeight - window.innerHeight;
+    
+    const scrollPosition = window.scrollY;
+    
+    const scrollPercentage = (scrollPosition / totalHeight) * 100;
+    
+    document.getElementById("scrollIndicator").style.width = scrollPercentage + "%";
+}
+
+window.addEventListener("scroll", updateScrollIndicator);
+
+// NAV
+
+const hamburgerButton = document.querySelector(".nav-toggler")
+const navigation = document.querySelector("nav")
+
+hamburgerButton.addEventListener("click", toggleNav)
+
+function toggleNav(){
+  hamburgerButton.classList.toggle("active")
+  navigation.classList.toggle("active")
+}
+
+// GO TOP
+
+const scrollToTopBtn = document.getElementById('scrollToTopBtn');
+
+window.addEventListener('scroll', function() {
+    if (window.pageYOffset > 100) { 
+        scrollToTopBtn.style.display = 'block';
+    } else {
+        scrollToTopBtn.style.display = 'none';
+    }
+});
+
+scrollToTopBtn.addEventListener('click', function() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'  // Défilement fluide
+    });
+});
+
+// LOADER
+
+window.addEventListener("load", function() {
+    let progressBar = document.getElementById('progress-bar');
+    let loader = document.getElementById('loader');
+    
+    let images = document.querySelectorAll('img');
+    let totalElements = images.length;
+    let loadedElements = 0;
+
+    function updateProgress() {
+        loadedElements++;
+        let progress = (loadedElements / totalElements) * 100;
+        progressBar.style.width = progress + '%';
+
+        if (loadedElements === totalElements) {
+            setTimeout(function() {
+                document.body.classList.add('loaded');
+            }, 500);
+        }
+    }
+
+    images.forEach(function(image) {
+        if (image.complete) {
+            updateProgress();
+        } else {
+            image.addEventListener('load', updateProgress);
+            image.addEventListener('error', updateProgress);
+        }
+    });
+});
+
 // SLIDE
 
 let currentIndex = 0;
