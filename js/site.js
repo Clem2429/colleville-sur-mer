4212d4d6ff88c5d14eab0e76357e893233703d68
clement <clement.legoube@gmail.com>
2024-09-29 19:18:47 +0200

    Loader + responsive

--- js/site.js
+++ js/site.js
@@ -28,3 +28,42 @@ scrollToTopBtn.addEventListener('click', function() {
         behavior: 'smooth'  // Défilement fluide
     });
 });
+
+// LOADER
+
+window.addEventListener("load", function() {
+    let progressBar = document.getElementById('progress-bar');
+    let loader = document.getElementById('loader');
+    
+    if (sessionStorage.getItem('pageLoaded')) {
+        document.body.classList.add('loaded');
+        return;
+    }
+
+    let images = document.querySelectorAll('img');
+    let totalElements = images.length;
+    let loadedElements = 0;
+
+    function updateProgress() {
+        loadedElements++;
+        let progress = (loadedElements / totalElements) * 100;
+        progressBar.style.width = progress + '%';
+
+        if (loadedElements === totalElements) {
+            setTimeout(function() {
+                document.body.classList.add('loaded');
+                
+                sessionStorage.setItem('pageLoaded', 'true');
+            }, 500); 
+        }
+    }
+
+    images.forEach(function(image) {
+        if (image.complete) {
+            updateProgress();
+        } else {
+            image.addEventListener('load', updateProgress);
+            image.addEventListener('error', updateProgress);
+        }
+    });
+});
